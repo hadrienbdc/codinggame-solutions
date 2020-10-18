94715afd9798632b7896703db59123c3a283bc46
Hadrien Baflast-Bienvenu <hbaflast@gmail.com>
2020-10-19 00:08:12 +0200

    push new solutions

--- multiplayer/codegolf/POWER OF THOR/solution.py
+++ multiplayer/codegolf/POWER OF THOR/solution.py
@@ -1,2 +1,2 @@
 X,Y,x,y=map(int,input().split())
-while 1:a,b=(X>x)-(X<x),(Y>y)-(Y<y);print(('S N'[1-b]+'E W'[1-a]).strip());x+=a;y+=b
+while 1:a,b=(X>x)-(X<x),(Y>y)-(Y<y);print((' SN'[b]+' EW'[a]).strip());x+=a;y+=b
